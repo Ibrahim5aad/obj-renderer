2bfb1bc24fc5e649050f78fb30200327c20e8583
Ibrahim Saad <ibrahimsaad419@gmail.com>
2019-08-07 23:58:55 +0200

    Add files via upload

---
+++ ObjRenderer/Object.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenGL;
+
+namespace ObjRenderer
+{
+    class Object
+    {
+        public readonly Vector3[] Vertices;
+        public readonly Vector2[] TextureVertices;
+        public readonly Vector3[] Normals;
+        public readonly int[] Indices;
+
+        public Object(Vector3[] vertices, 
+                        Vector2[] textureVertices, 
+                        Vector3[] normals, 
+                        int[] indices)
+        {
+            this.Vertices = vertices;
+            this.TextureVertices = textureVertices;
+            this.Normals = normals;
+            this.Indices = indices;
+
+        }
+    }
+}
---
+++ ObjRenderer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenTK;
+
+namespace ObjRenderer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var obj = ObjLoader.Loader(@"C:\Users\Saad\Desktop\Desktop\RevitModel.obj");
+            //Console.WriteLine(obj.Normals[0]);
+            var r = new ObjRenderer();
+            
+        }
+    }
+}
