4479acf057bb42cd8739e4a18d72c20524859ca7
Ibrahim5aad <ibrahimsaad419@gmail.com>
2021-06-16 21:07:11 +0200

    .ignore file

--- ObjRenderer/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
+++ ObjRenderer/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace ObjRenderer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ObjRenderer;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ObjRenderer;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
